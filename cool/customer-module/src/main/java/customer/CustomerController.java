a4a55c677423d7ab60d1a8610214ee087f41d63f
gcdinesh <gcdinesh14@gmail.com>
2019-01-18 20:18:35 +0530

    created dockerfile and docker compose

--- cool/customer-module/src/main/java/customer/CustomerController.java
+++ cool/customer-module/src/main/java/customer/CustomerController.java
@@ -1,5 +1,7 @@
 package customer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,18 @@ public class CustomerController {
 
     @GetMapping("/allshops")
     public ResponseEntity<String> allShops(){
-        final String uri = "http://localhost:8080/allshops";
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity(null, httpHeaders), String.class);
-        return result;
+        try {
+            final String uri = "http://10.0.2.15:8080/allshops";
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity(null, httpHeaders), String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            String response = mapper.writeValueAsString(result);
+            return new ResponseEntity<String>(response, new HttpHeaders(), result.getStatusCode());
+        }catch (Exception e){
+
+        }
+        return null;
     }
 }
